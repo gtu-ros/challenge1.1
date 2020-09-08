884d5c98070390150a0ba897a81cd106ce033bd8
Harun Albayrak <52447324+harunalbayrak@users.noreply.github.com>
2020-09-08 20:06:40 +0300

    Added my go.py file

--- src/go.py
+++ src/go.py
@@ -1,125 +1,78 @@
 #!/usr/bin/env python
-
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-import math
-
-TOLERANCE = 0.1
-
-
-def init_movement(diff):
-
-    vel_msg = Twist()
-
-    vel_msg.linear.x = TOLERANCE
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0
-    return vel_msg
+from math import pow, atan2, sqrt
 
 
 class TurtleBot:
-    def __init__(self):
 
+    def __init__(self):
         rospy.init_node('turtlebot_controller', anonymous=True)
-        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
-                                                  Twist,
-                                                  queue_size=10)
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose,
-                                                self.update_pose)
+        self.velocity_publisher = rospy.Publisher(
+            '/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pose_subscriber = rospy.Subscriber(
+            '/turtle1/pose', Pose, self.update_pose)
 
         self.pose = Pose()
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(5)
 
     def update_pose(self, data):
         self.pose = data
-        self.pose.x = round(self.pose.x, 2)
-        self.pose.y = round(self.pose.y, 2)
+        self.pose.x = round(self.pose.x, 5)
+        self.pose.y = round(self.pose.y, 5)
 
-    def init_rotation(self, diff_y):
+    def euclidean_distance(self, goal_pose):
+        return sqrt(pow(float((goal_pose.x - self.pose.x)), 2) + pow(float((goal_pose.y - self.pose.y)), 2))
+
+    def angular_vel(self, goal_pose):
+    	my_theta = atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+
+    	return my_theta - self.pose.theta
+        
+
+    def move2goal(self):
+        goal_pose = Pose()
         vel_msg = Twist()
+
+        x = 0
+
+        while True:  
+            if(x == 1):
+                print("Wrong Input. Try Again: \n")
+            try:
+                goal_pose.x = float(input("Enter X : "))
+                goal_pose.y = float(input("Enter Y : "))
+            except Exception as e:
+                continue
+            if(goal_pose.x > 0 and goal_pose.x < 11 and goal_pose.y > 0 and goal_pose.y < 11):  
+                break  
+            else:
+                x = 1
+        
 
         vel_msg.linear.x = 0
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
-        # to find out fastest rotation
-        vel_msg.angular.z = TOLERANCE
-        if (diff_y > 0):
-            vel_msg.angular.z = TOLERANCE
-        else:
-            vel_msg.angular.z = -TOLERANCE
-        return vel_msg
+        vel_msg.angular.z = self.angular_vel(goal_pose)
 
-    def correctAngle(self, diff):
-        return abs(self.pose.theta - diff.theta) < TOLERANCE
+        self.velocity_publisher.publish(vel_msg)
+        rospy.sleep(2.5)
 
-    def isReached(self, distance):
-        x = 5.54 - abs(self.pose.x)
-        y = 5.54 - abs(self.pose.y)
-
-        d = math.sqrt(x * x + y * y)
-
-        return (d > distance)
-
-    def move2goal(self):
-        goal_pose = Pose()
-        diff = Pose()
-
-        goal_pose.x = float(input("Set your x goal: "))
-        goal_pose.y = float(input("Set your y goal: "))
-
-        self.rate.sleep()
-        diff.x = goal_pose.x - self.pose.x
-        diff.y = goal_pose.y - self.pose.y
-        diff.theta = math.atan(diff.y / diff.x)
-        if (diff.x < 0):
-            if (diff.y > 0):
-                diff.theta = math.pi + diff.theta
-            else:
-                diff.theta = -math.pi + diff.theta
-
-        print(diff)
-
-        vel_msg = self.init_rotation(diff.y)
-
-        # rotates until finds correct angle to move
-        while not self.correctAngle(diff):
-            self.rate.sleep()
-
-            print("x: ", self.pose.x)
-            print("y: ", self.pose.y)
-            print("theta: ", self.pose.theta)
-
+        while self.euclidean_distance(goal_pose) > 0.40:
+            vel_msg.linear.x = 1.5
+            vel_msg.angular.z = 0.0
             self.velocity_publisher.publish(vel_msg)
+            rospy.loginfo("euclidean_distance : " + str(self.euclidean_distance(goal_pose)) + "\n")
 
-            self.rate.sleep()
-
-        # move euclidian distance
-        vel_msg = init_movement(diff)
-
-        distance = math.sqrt(diff.x * diff.x + diff.y * diff.y)
-
-        while not self.isReached(distance):
-            self.rate.sleep()
-
-            print("x: ", self.pose.x)
-            print("y: ", self.pose.y)
-            print("theta: ", self.pose.theta)
-
-            self.velocity_publisher.publish(vel_msg)
-
-            self.rate.sleep()
-
-        rospy.spin()
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
 
 
 if __name__ == '__main__':
-
     try:
         x = TurtleBot()
         x.move2goal()
